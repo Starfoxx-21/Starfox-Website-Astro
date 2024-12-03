b4e89cc9dc61a3686a7854e4ca248755aa5f90e0
Christianna A. <c.anastasiadou@hotmail.com>
2024-12-04 01:38:36 +0200

    Added lang support i18n

---
+++ src/i18n/ui.js
@@ -0,0 +1,33 @@
+export const languages = {
+    en: "English",
+    el: "Greek",
+  };
+  
+  export const defaultLang = "en";
+  
+export const ui = {
+  en: {
+    "about": "About",
+    "contact": "Contact",
+    "lang": "en",
+    "downloadresume": "Download Resume",
+    "aboutme": "About me",
+    "aboutmetext": "My name is Christianna, and I live in Athens. I used to work as an accountant, but now I’m determined to pursue my goal of becoming a web developer. Over the next five years, I hope to grow my programming skills and work on interesting projects. What I appreciated most during the interview was realizing that the team behind this company is made up of young, creative individuals. I am excited about the opportunity to learn and work with the latest technologies and to create websites that are both functional and visually appealing.",
+    "contactphraseone": "Get in",
+    "contactphrasetwo": "Touch",
+    "contactbuttonphrase": "Contact me",
+    "findsocial": "Find me on:",
+  },
+  el: {
+    "about": "Σχετικά",
+    "contact": "Επικοινωνία",
+    "lang": "el",
+    "downloadresume": "Λήψη Βιογραφικού",
+    "aboutme": "Λίγα Λόγια για Εμένα",
+    "aboutmetext": "Με λένε Χριστιάννα και ζω στην Αθήνα. Δούλευα ως λογίστρια, αλλά τώρα είμαι αποφασισμένη να επιδιώξω τον στόχο μου να γίνω web developer. Τα επόμενα πέντε χρόνια, ελπίζω να αναπτύξω τις προγραμματιστικές μου δεξιότητες και να δουλέψω σε ενδιαφέροντα έργα. Αυτό που εκτίμησα περισσότερο κατά τη διάρκεια της συνέντευξης ήταν ότι συνειδητοποίησα πως η ομάδα πίσω από αυτή την εταιρεία αποτελείται από νέους, δημιουργικούς ανθρώπους. Είμαι ενθουσιασμένη για την ευκαιρία να μάθω και να δουλέψω με τις πιο σύγχρονες τεχνολογίες, δημιουργώντας ιστοσελίδες που είναι τόσο λειτουργικές όσο και οπτικά ελκυστικές.",
+    "contactphraseone": "Επικοινωνήστε ",
+    "contactphrasetwo": "μαζί μου",
+    "contactbuttonphrase": "Μάθετε περισσότερα",
+    "findsocial": "Θα με βρείτε εδώ:"
+  },
+};

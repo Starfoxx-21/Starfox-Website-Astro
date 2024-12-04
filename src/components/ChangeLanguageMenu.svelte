063d0170e6961a4dcc5916ad08d3e6a264b57d83
Christianna A. <c.anastasiadou@hotmail.com>
2024-12-04 23:43:33 +0200

    Added functionality to the language menu for big screens

---
+++ src/components/ChangeLanguageMenu.svelte
@@ -0,0 +1,6 @@
+<div class="absolute bg-pink-light border-solid border-pink-dark border-2 rounded-md w-10 text-center">
+  <ul class="my-1">
+    <li ><a href="/" class="hover:bg-pink-dark rounded-md px-1">en</a></li>
+    <li><a href="/el" class="hover:bg-pink-dark rounded-md px-1">ελ</a></li>
+  </ul>
+</div>
---
+++ src/components/DropdownMenu.svelte
@@ -0,0 +1,15 @@
+<script>
+    import dropdown from "/public/images/dropdown-arrow.svg";
+
+    export let buttonName;
+
+    let showContent = false;
+
+</script>
+
+<button onclick={()=>{showContent = !showContent}} class="lang-button relative flex flex-row items-center w-5">
+    {buttonName} <img src={dropdown.src} alt="dropdown">
+</button>
+{#if showContent}
+    <slot></slot>
+{/if}

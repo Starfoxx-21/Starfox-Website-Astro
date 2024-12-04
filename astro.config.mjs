37a8ecbbd9ee91f547c8876d1f8e8d5baa725a73
Christianna A. <c.anastasiadou@hotmail.com>
2024-12-05 01:52:44 +0200

    Added github yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,15 @@ import tailwind from '@astrojs/tailwind';
 
 import svelte from '@astrojs/svelte';
 
+const githubURL = "https://starfox-website-astro.github.io";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   i18n: {
     locales: ["el", "en"],
     defaultLocale: "en",
-  }
+  },
+  site: githubURL,
+  base: 'my-repo',
 });

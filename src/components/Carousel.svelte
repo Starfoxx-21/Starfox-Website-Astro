6163a65717c64decac5cd054408a714ecfcfe9bf
Christianna A. <c.anastasiadou@hotmail.com>
2024-12-04 16:21:20 +0200

    Added  content into the Project section & functionality. Also added about/contact/project pages

---
+++ src/components/Carousel.svelte
@@ -0,0 +1,45 @@
+<script>
+  let images = [
+    '/images/jewelry-commerce.png',
+    '/images/starfox-portofolio.png',
+    '/images/recipes-website.png',
+  ];
+
+  let currentIndex = 0;
+
+  const next = () => {
+    if (currentIndex < images.length - 1) {
+      currentIndex = currentIndex + 1;
+    } else {
+      currentIndex = 0;
+    }
+  };
+
+  const prev = () => {
+    if (currentIndex > 0) {
+      currentIndex = currentIndex - 1;
+    } else {
+      currentIndex = images.length - 1;
+    }
+  };
+</script>
+
+<div class="relative max-w-[1100px] overflow-hidden mt-1">
+  <div class="w-full transition-transform duration-500 ease-in-out">
+      <img src={images[currentIndex]} alt="carousel" class="w-full object-cover rounded-lg shadow-lg"/>
+  </div>
+  <button
+    onclick={prev} class="absolute top-1/2 left-0 transform -translate-y-1/2 px-4 py-2 bg-pink-dark text-white rounded-full hover:bg-pink-light" aria-label="Previous image">&#10094;
+  </button>
+  <button
+    onclick={next} class="absolute top-1/2 right-0 transform -translate-y-1/2 px-4 py-2 bg-pink-dark text-white rounded-full hover:bg-pink-light" aria-label="Next image">&#10095;
+  </button>
+</div>
+
+<div class="flex justify-center space-x-2 mt-4">
+  {#each images as _, index}
+    <button
+      class="w-3 h-3 bg-pink-dark rounded-full" class:bg-opacity-50={index !== currentIndex} onclick={() => (currentIndex = index)} aria-label={`Go to image ${index + 1}`}>
+    </button>
+  {/each}
+</div>

21b460875ba9e6943e64cf2ceeab20c9323131c7
Christianna A. <c.anastasiadou@hotmail.com>
2024-12-06 00:51:30 +0200

    Fixed logo to toggle on both english and greek. Also made it always visible on all pages except homepage

--- src/components/ToggleHeaderLogo.svelte
+++ src/components/ToggleHeaderLogo.svelte
@@ -3,27 +3,43 @@
   import starfox from "../images/starfox.png";
   import { onMount } from 'svelte';
 
-  let isVisible = false;
+  let isVisible = true;
+  let shouldToggle = false;
 
-  const checkScroll = () => {
-    if (window.scrollY > 100) {
-      isVisible = true;
-    } else {
-      isVisible = false;
-    }
-  };
-  
   onMount(() => {
-    window.addEventListener('scroll', checkScroll);
+    const path = window.location.pathname;
+    console.log(path)
+
+    if (path === "/Starfox-Website-Astro/" || path === "/Starfox-Website-Astro/el") {
+      shouldToggle = true;
+      isVisible = false;
+    } else {
+      isVisible = true;
+    }
+
+    const checkScroll = () => {
+      if (window.scrollY > 100) {
+        isVisible = true;
+      } else {
+        isVisible = false;
+      }
+    };
+
+    if (shouldToggle) {
+      window.addEventListener('scroll', checkScroll);
+    }
+
     return () => window.removeEventListener('scroll', checkScroll);
+
   });
 </script>
 
-<div>
-  <a class="flex flex-row flex-1 items-center" href="/">
-    <img class="w-14 transition-all duration-300 ease-in-out transform" src={logo.src} alt="starfox logo" class:opacity-0={!isVisible}
-    class:opacity-100={isVisible} class:scale-50={!isVisible} class:scale-100={isVisible}/>
-    <img class="w-52 transition-all duration-300 ease-in-out transform" src={starfox.src} alt="starfox" class:opacity-0={!isVisible}
-    class:opacity-100={isVisible} class:scale-50={!isVisible} class:scale-100={isVisible}>
+<div class="transition-all duration-300 ease-in-out transform" class:opacity-0={!isVisible && shouldToggle} 
+class:opacity-100={isVisible || !shouldToggle}
+class:scale-50={!isVisible && shouldToggle}
+class:scale-100={isVisible || !shouldToggle}>
+  <a class="flex flex-row flex-1 items-center" href="/Starfox-Website-Astro/">
+    <img class="w-14" src={logo.src} alt="starfox logo"/>
+    <img class="w-52" src={starfox.src} alt="starfox">
   </a>
 </div>

d3878f669cf0e8d220b2f20376572ac24a13867c
Christianna A. <c.anastasiadou@hotmail.com>
2024-12-06 15:24:35 +0200

    Added functionality on download button

--- src/i18n/utils.js
+++ src/i18n/utils.js
@@ -2,7 +2,7 @@ import { ui, defaultLang } from './ui';
 
 export function getLangFromUrl(url) {
   const base = import.meta.env.BASE_URL
-  const relativePath = url.pathname.replace(base, ''); // Remove base path
+  const relativePath = url.pathname.replace(base, '');
   const [, lang] = relativePath.split('/');
   if (lang in ui) return lang;
   return defaultLang;
